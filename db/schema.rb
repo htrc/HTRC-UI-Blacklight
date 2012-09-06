299e7244c67c081385d05eb845a052949b9e496d
Kirk Hess <kirkhess@illinois.edu>
2012-09-06 21:00:11 +0000

    adding db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120816204130) do
+ActiveRecord::Schema.define(:version => 20120903123039) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
