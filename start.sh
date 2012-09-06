2de35a91e69de24d2906c956d705806526c98f1c
Kirk Hess <kirkhess@illinois.edu>
2012-09-06 20:29:47 +0000

    checkin start.sh changes for using passenger

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo Starting Blacklight in the background...
-RAILS_RELATIVE_URL_ROOT="/blacklight" rails server --environment=production --daemon
+echo Starting Blacklight in the background w/passenger...
+#RAILS_RELATIVE_URL_ROOT="/blacklight" rails server --environment=production --daemon
+RAILS_RELATIVE_URL_ROOT="/blacklight" passenger start --environment=production --daemon
