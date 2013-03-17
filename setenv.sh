d25ff406f7e66dc56c560aca0b6c8d09062729bf
Craig Willis <willis8@illinois.edu>
2013-03-17 04:23:34 +0000

    Added setenv script

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+# Environment for htrc6
+export PATH=/home/drhtrc/htrc-blacklight/bin:/home/drhtrc/htrc-blacklight/gems/bin:$PATH
+export GEM_HOME=/home/drhtrc/htrc-blacklight/
+export RUBYLIB=/home/drhtrc/htrc-blacklight/lib
