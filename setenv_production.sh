5e4a653d029c2085c8afb61e5e32bed718d863fc
Craig Willis <willis8@illinois.edu>
2013-08-14 15:41:30 +0000

    Created separate setenv for each environment.

--- setenv_production.sh
+++ setenv_production.sh
@@ -1,4 +1,4 @@
-# Environment for htrc6
+# Environment for production
 export PATH=/home/drhtrc/htrc-blacklight/bin:/home/drhtrc/htrc-blacklight/gems/bin:$PATH
 export GEM_HOME=/home/drhtrc/htrc-blacklight/
 export RUBYLIB=/home/drhtrc/htrc-blacklight/lib
---
+++ setenv_sandbox.sh
@@ -0,0 +1,4 @@
+# Environment for sandbox
+export PATH=/data1/htrc/blacklight/bin:/data1/htrc/blacklight/gems/bin:$PATH
+export GEM_HOME=/data1/htrc/blacklight/
+export RUBYLIB=/data1/htrc/blacklight/lib
