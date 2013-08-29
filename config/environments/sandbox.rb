14e75004f2f96a2ca3b42ca79136e6aacfdbed77
Colleen Fallaw <mfall3@illinois.edu>
2013-08-29 13:26:33 +0000

    added asset_controller information for the Sandbox environment

--- config/environments/sandbox.rb
+++ config/environments/sandbox.rb
@@ -62,7 +62,11 @@ BlacklightHtrc::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  #Sets the host for the assets
+  config.action_controller.asset_host = "https://sandbox.htrc.illinois.edu:8443"
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
 end
