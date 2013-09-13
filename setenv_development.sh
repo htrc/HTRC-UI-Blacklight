f4da6ca3b204dd25989b13e00b562a420448ed42
Colleen Fallaw <mfall3@illinois.edu>
2013-09-13 19:53:23 +0000

    adjust path

--- setenv_development.sh
+++ setenv_development.sh
@@ -1,5 +1,5 @@
 # Environment for development
-export PATH=$HOME/.rbenv/bin:/home/drhtrc/htrc-blacklight/bin:/home/drhtrc/htrc-blacklight/gems/bin:$PATH
+export PATH=$HOME/.rbenv/bin:/home/drhtrc/htrc-blacklight/bin:$PATH
 #export GEM_HOME=/home/drhtrc/htrc-blacklight/
 export RUBYLIB=/home/drhtrc/htrc-blacklight/lib
 eval "$(rbenv init -)"
