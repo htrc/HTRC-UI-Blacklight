58b5f6a13559e813ce0cc24164a546135dce8ea1
Colleen Fallaw <mfall3@illinois.edu>
2013-09-13 19:01:09 +0000

    change path and remove GEM_HOME variable to use rbenv

--- setenv_development.sh
+++ setenv_development.sh
@@ -1,4 +1,5 @@
 # Environment for development
-export PATH=$PATH:/home/drhtrc/htrc-blacklight/bin
+export PATH=$HOME/.rbenv/bin:/home/drhtrc/htrc-blacklight/bin:/home/drhtrc/htrc-blacklight/gems/bin:$PATH
 #export GEM_HOME=/home/drhtrc/htrc-blacklight/
 export RUBYLIB=/home/drhtrc/htrc-blacklight/lib
+eval "$(rbenv init -)"
