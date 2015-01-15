0a1c46fad1d89bdc113861062c7f8e5788670dc5
mfall3 <mfall3@illinois.edu>
2015-01-15 08:19:19 -0600

    update asset host

--- setenv_production.sh
+++ setenv_production.sh
@@ -1,4 +1,6 @@
 # Environment for production
 export PATH=/home/drhtrc/htrc-blacklight/bin:/home/drhtrc/htrc-blacklight/gems/bin:$PATH
+export PATH=$HOME/.rbenv/bin:$PATH
 export GEM_HOME=/home/drhtrc/htrc-blacklight/
 export RUBYLIB=/home/drhtrc/htrc-blacklight/lib
+eval "$(rbenv init -)"
