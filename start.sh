0007015c25782273dcfc745631150cb2e2c6ef5c
mfall3 <mfall3@illinois.edu>
2015-03-09 14:03:21 -0500

    add environment-specific start scripts

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Starting Blacklight in the background w/passenger...
-#RAILS_RELATIVE_URL_ROOT="/blacklight" rails server --environment=production --daemon
-RAILS_RELATIVE_URL_ROOT="/blacklight" passenger start --environment=development --daemon
